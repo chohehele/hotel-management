def4fe5e8790c5bd8d3312a849bac46f4e1e52be
Ryan Lee <ryanyinlee@protonmail.com>
2022-02-08 10:43:10 -0800

    stable server running, db active

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 'use strict';
 require('dotenv').config();
+
+const { db } = require('./lib/models');
+const server = require('./lib/server.js');
+const port = process.env.PORT || 3000;
+
+db.sync().then(() => {
+  server.start(port);
+});

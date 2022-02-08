d252415b8df2ad315ef896dfedf47867d01fcbca
Ryan Lee <ryanyinlee@protonmail.com>
2022-02-08 11:53:29 -0800

    added test templates

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -17,15 +17,78 @@ describe('What the test do', () => {
 })
 
 // Should be able to create an account
+describe('What the test do', () => {
+
+    it('should do this.', async () => {
+        
+
+       
+        expect('some response').toEqual('some response');
+    })
+})
 
 // Should be able to log into that account
+describe('What the test do', () => {
+
+    it('should do this.', async () => {
+        
+
+       
+        expect('some response').toEqual('some response');
+    })
+})
 
 // Should be able to enter guest info in
+describe('What the test do', () => {
+
+    it('should do this.', async () => {
+        
+
+       
+        expect('some response').toEqual('some response');
+    })
+})
 
 // Should fail when guest info is incomplete
+describe('What the test do', () => {
+
+    it('should do this.', async () => {
+        
+
+       
+        expect('some response').toEqual('some response');
+    })
+})
 
 // Should be able to make a reservation for a future date
+describe('What the test do', () => {
+
+    it('should do this.', async () => {
+        
+
+       
+        expect('some response').toEqual('some response');
+    })
+})
 
 // Room will be unavailable for future date
+describe('What the test do', () => {
+
+    it('should do this.', async () => {
+        
+
+       
+        expect('some response').toEqual('some response');
+    })
+})
 
 // Should prioritize guests based on past stays
+describe('What the test do', () => {
+
+    it('should do this.', async () => {
+        
+
+       
+        expect('some response').toEqual('some response');
+    })
+})

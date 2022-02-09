b9bea82785813e8fcef32b680177f959d15f3ab4
Ryan Lee <ryanyinlee@protonmail.com>
2022-02-08 23:27:15 -0800

    stable login

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -6,89 +6,89 @@ const request = supertest(server.app);
 
 // Test template for copy/paste.
 
-describe('What the test do', () => {
+// describe('What the test do', () => {
 
-    it('should do this.', async () => {
+//     it('should do this.', async () => {
         
 
        
-        expect('some response').toEqual('some response');
-    })
-})
+//         expect('some response').toEqual('some response');
+//     })
+// })
 
-// Should be able to create an account
-describe('What the test do', () => {
+// // Should be able to create an account
+// describe('What the test do', () => {
 
-    it('should do this.', async () => {
+//     it('should do this.', async () => {
         
 
        
-        expect('some response').toEqual('some response');
-    })
-})
+//         expect('some response').toEqual('some response');
+//     })
+// })
 
-// Should be able to log into that account
-describe('What the test do', () => {
+// // Should be able to log into that account
+// describe('What the test do', () => {
 
-    it('should do this.', async () => {
+//     it('should do this.', async () => {
         
 
        
-        expect('some response').toEqual('some response');
-    })
-})
+//         expect('some response').toEqual('some response');
+//     })
+// })
 
-// Should be able to enter guest info in
-describe('What the test do', () => {
+// // Should be able to enter guest info in
+// describe('What the test do', () => {
 
-    it('should do this.', async () => {
+//     it('should do this.', async () => {
         
 
        
-        expect('some response').toEqual('some response');
-    })
-})
+//         expect('some response').toEqual('some response');
+//     })
+// })
 
-// Should fail when guest info is incomplete
-describe('What the test do', () => {
+// // Should fail when guest info is incomplete
+// describe('What the test do', () => {
 
-    it('should do this.', async () => {
+//     it('should do this.', async () => {
         
 
        
-        expect('some response').toEqual('some response');
-    })
-})
+//         expect('some response').toEqual('some response');
+//     })
+// })
 
-// Should be able to make a reservation for a future date
-describe('What the test do', () => {
+// // Should be able to make a reservation for a future date
+// describe('What the test do', () => {
 
-    it('should do this.', async () => {
+//     it('should do this.', async () => {
         
 
        
-        expect('some response').toEqual('some response');
-    })
-})
+//         expect('some response').toEqual('some response');
+//     })
+// })
 
-// Room will be unavailable for future date
-describe('What the test do', () => {
+// // Room will be unavailable for future date
+// describe('What the test do', () => {
 
-    it('should do this.', async () => {
+//     it('should do this.', async () => {
         
 
        
-        expect('some response').toEqual('some response');
-    })
-})
+//         expect('some response').toEqual('some response');
+//     })
+// })
 
-// Should prioritize guests based on past stays
-describe('What the test do', () => {
+// // Should prioritize guests based on past stays
+// describe('What the test do', () => {
 
-    it('should do this.', async () => {
+//     it('should do this.', async () => {
         
 
        
-        expect('some response').toEqual('some response');
-    })
-})
+//         expect('some response').toEqual('some response');
+//     })
+// })
